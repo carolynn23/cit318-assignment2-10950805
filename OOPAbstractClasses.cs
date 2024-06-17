00b8c184a2e89cc5d16c1dde60d15240a5115974
carolynn23 <102730717+carolynn23@users.noreply.github.com>
2024-06-17 16:24:31 +0000

    Create OOPAbstractClasses.cs

---
+++ OOPAbstractClasses.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace OOPAbstractClasses
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(5);
+            Shape rectangle = new Rectangle(4, 7);
+
+            Console.WriteLine($"Area of Circle: {circle.GetArea()}");
+            Console.WriteLine($"Area of Rectangle: {rectangle.GetArea()}");
+        }
+    }
+}
